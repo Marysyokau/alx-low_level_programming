d771062f79802326e39180a37d759a7f8a99cfeb
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-15 11:42:41 -0700

    4-put

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 #include<unistd.h>
-/**
- * main - Entry point
- *
- * Return Always 1 (Success)
- */
+/*using other form of output*/
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
