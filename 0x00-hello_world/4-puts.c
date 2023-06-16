a1cfdf6a437617c114a5d2c58a95b9b929fa82f0
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-16 05:22:28 -0700

    commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
- * puts(programming is like putting a puzzle)
- * Return: Always 0(Success)
+ * puts(print option of a line)
+ * Retutn: Always 0(Success)
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);		
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
