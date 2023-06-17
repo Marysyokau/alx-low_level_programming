f309bd08dadb88497ad8be9b4daaed0967faa206
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-17 06:34:31 -0700

    my commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point 
- * all single digit numbers of base 10 starting from 0
+ * main - Entry point of process
+ * all single numbers of base 10 starting from 0
  * Return: Always 0(Success)
- */ 
+ */
 int main(void)
 {
-         int i;
+	int i;
 
-        for (i = 0; i < 10; i++)
-        {
-                putchar(i);
-        }
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
 	putchar('\n');
 	return (0);
 }
