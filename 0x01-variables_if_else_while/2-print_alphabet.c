759676aec44af86d684210bc82c4e75a5279cc1a
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-17 05:52:41 -0700

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point 
- * I sometimes suffer from insomnia. And when I can't fall asleep
+ * main - Entry point of process
+ * I sometimes suffer from imsomnia
  * Return: Always 0(Success)
- */ 
+ */
 int main(void)
 {
-        char alph[26] = "abcdefghijklmnopqrstuvwxyz";
-        int i;
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-        for (i = 0; i < 26; i++)
-        {
-               putchar(alph[i]);
-        }
+	for(i = 0; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
 	putchar('\n');
 	return (0);
 }
