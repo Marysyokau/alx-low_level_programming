d0430160b2a5ce9b0b7f6c8978809e5b031446ed
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-17 07:00:13 -0700

    my commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 /**
- * main - Entry point 
- * prints all the numbers of base 16 in lowercase
+ * main - Entry point of program
+ * prints all numbers of base 16 in lower case
  * Return: Always 0(Success)
- */ 
+ */
 int main(void)
 {
-         int i;
-
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-        }
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 0; i <6; i++)
+	{
+		putchar(i + 'a');
+	}
 	putchar('\n');
 	return (0);
 }
