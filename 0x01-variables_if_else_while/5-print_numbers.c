95a7a58933f0a43fc8fa768100fe306357769306
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-17 06:12:06 -0700

    my commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point 
+ * main - Entry point of process
  * all single digit numbers of base 10 starting from 0
  * Return: Always 0(Success)
  */ 
