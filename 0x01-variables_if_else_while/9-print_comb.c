a94f024cbdbd522dcab72c0b1b88304f846a2091
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-17 07:15:23 -0700

    my commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point 
- * single-digit numbers
+ * main - Entry point of a program
+ * single digits numbers
  * Return: Always 0(Success)
- */ 
+ */
 int main(void)
 {
-         int i;
+	int i;
 
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-                if (i != 57)
-                {
-                        putchar(',');
+	for (i = 0; i <10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
 			putchar(' ');
-                }
-        }
+		}
+	}
 	putchar('\n');
 	return (0);
 }
