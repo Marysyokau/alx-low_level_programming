de70e2e86c26818406e19735d465fd46f309f5fa
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-18 03:07:11 -0700

    My commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Entry point 
- * prints all possible combinations of two two-digit numbers
+ * main - Entry point of program
+ * all possible combination of two two digit numbers
  * Return: Always 0(Success)
- */ 
+ */
 int main(void)
 {
-         int i,j;
+	int i, j, k, l;
 
-        for (i = 0; i < 100; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 1000; j++)
+		for (j = 0; j < 9; j++)
 		{
-			if (i < j)
+			for (k = 0; k < 10; k++)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if(i != 98 || j != 99)
+				for (l = 1; l < 10; l++)
 				{
-					putchar(',');
+					putchar(i + '0');
+					putchar(j + '0');
 					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+
+					if (i < 9 || j < 8 || k < 9 || l < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
