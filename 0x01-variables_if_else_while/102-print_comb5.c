c3d114746ba107983d741b25be0fe4eb3303c32e
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-18 03:42:57 -0700

    My commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,28 +7,22 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int num1, num2;
 
-	for (i = 0; i < 10; i++)
+	for (num1= 0; num1 <= 98 ; num1++)
 	{
-		for (j = 0; j < 9; j++)
+		for (num2 =num1 + 1; num2 < 99; num2++)
 		{
-			for (k = 0; k < 10; k++)
-			{
-				for (l = j + 1; l < 10; l++)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-					if (i < 9 || j < 8 || k < 9 || l < 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+			if (num1 < 98 || num2 < 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
