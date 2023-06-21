ff9e04c9c51fece9bd558ce64bcf7bb8a79bbabd
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-21 00:08:34 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * prints alphabet in small letters
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <  'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
