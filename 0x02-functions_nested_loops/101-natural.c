bb6dbca6f4783eafdc5a4b7edb35571f653aef6b
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-21 15:34:00 -0700

    my commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	int i , z = 0;
+	int i, z = 0;
 
 	while (i < 1024)
 	{
-		if ((i % 3 ==0) ||(i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			z += i;
-		}
+		}i
 		i++;
 	}
 	printf("%d\n", z);
