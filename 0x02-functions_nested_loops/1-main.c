bd4b09d55aa234920648c41542585fb4a63a4a8d
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-21 00:37:06 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - entry point
- * print alphabets
- * Return: Always 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	print_alphabet();
