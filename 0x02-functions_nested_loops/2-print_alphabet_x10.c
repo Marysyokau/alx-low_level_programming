eafe3c21c0b2f99eba01b165e9463ece11472d48
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-21 00:26:45 -0700

    ./2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * print alphabet 10 times
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+		i++;
+	}
+}
