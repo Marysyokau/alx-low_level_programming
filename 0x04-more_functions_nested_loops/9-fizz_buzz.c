3f947a4430a1490767a46793ca4117e5730e2e27
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-23 14:02:51 -0700

    Another commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,31 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * main - prints equivalent values for multiples
- * Return: Always 0(Success)
- */
+
+/** main - prints equivalent values for multiples
+ * Description: FizzBuzz
+ * Return: 0
+ */ 
 
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 1; i <= 100; i++)
+	for (num = 1; num <= 100; ++num)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz");			
-		}
-		else if (i % 3 == 0)
+		if (num % 3 == 0 && !(num % 5 == 0))
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
-		{
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
-		}
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
 		else
-		{
-			printf("%d", i);
-		}
-		if (i != 100)
-		{
+			printf("%d", num);
+		if (num != 100)
 			printf(" ");
-		}
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
