a20e400424a580287020116285541336290569cd
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-23 13:38:12 -0700

    Another commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,24 +2,19 @@
 
 /**
  * print_most_numbers - print numbers
- * Description: except 2 and 4
- * Return: The numbers
+ * Description: excluding 2 and 4
+ * Return: equivalent values
  */
 
 void print_most_numbers(void)
 {
-	int x = 0;
+	int num;
 
-	for(; x <= 9; x++)
+	for (num = 0; num <= 9; ++num)
 	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
 	}
 	_putchar('\n');
 }
