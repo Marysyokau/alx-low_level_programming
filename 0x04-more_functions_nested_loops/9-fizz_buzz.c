3487895cd1bf9d5ecb47e979618126b6a2153cb9
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-23 15:04:30 -0700

    my commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,8 @@
 
 int main(void)
 {
-	int i;
+	int i = 1;
 	
-	i = 1;
 	while (i <= 100)
 	{
 		if (i % 15 == 0)
@@ -23,7 +22,7 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			if (i == 10)
+			if (i == 100)
 			{
 				printf("Buzz");
 			}
