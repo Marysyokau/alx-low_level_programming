60d82e7f49ccaa1bb2fe97713484e98c12b3a7c3
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-26 13:09:01 -0700

    0x05-pointers_arrays_strings files

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,13 +11,15 @@
 
 int main(void)
 {
-	char password[14];
+	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
+
 	srand(time(0));
+
 	while (sum < 2772)
 	{
-		password[index] =rand % 94;
-		sum = password[index++];
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
 	password[index] = '\0';
 	if (sum != 2772)
