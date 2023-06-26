6c9f8d089b634805f2d4af3840ac424927b4f53c
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-06-26 02:25:33 -0700

    redo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - pointers to variables
+ * @n: checker digit
+ * a pointer to an int as parameter
+ * Return: 0(Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
