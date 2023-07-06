6c23071bb9b430aa50e9e77f68080e243a4f80a8
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-07-06 13:38:52 -0700

    square root of natural numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,39 @@
 #include "main.h"
 
 /**
- *  _sqrt_recursion - natural square root of a number.
- *  @n: input number
- *  Return: squareroot
- */
-
-int _sqrt_recursion(int n)
+ * sqtChecker - computes square root recursively doing binary search
+ * @min: inital number
+ * @max: last number within the limit of number
+ * @m: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+int sqtChecker(int min, int max, int m)
 {
-	int m;
+	long int guess;
 
+	if (max >= min)
+	{
+		guess = min + (max - min) / 2;
+		if (guess * guess == m)
+			return (guess);
+/* following binary search */
+		if (guess * guess > m)
+			return (sqtChecker(min, guess - 1, m));
+		if (guess * guess < m)
+			return (sqtChecker(guess + 1, max, m));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+	int _sqrt_recursion(int n)
+{
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else
-	{
-		if (n == 1)
-		{
-			return (1);
-		}
-		else
-		{
-			for (m = 0; m * m == n; m++)
-			{
-				return (m);	
-			}
-		}
-	}
+	if (n == 0)
+		return (0);
+	return (sqtChecker(1, n, n));
 }
