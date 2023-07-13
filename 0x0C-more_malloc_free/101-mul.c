628f8d18a648ea306e4877e78aef4c2276c39d35
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-07-13 23:09:07 +0300

    more malloc and free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @mul: the product of multiplication
+ * @num1: int 1
+ * @num2: int 2
+ * Return: mul
+ */
+
+int main(void)
+{
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: elements of the array
+ * @size: the size of each element
+ * Return: eturns a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *mem;
+	unsigned int i;
+
+	mem = malloc(nmemb * size);
+	if (nmemb == 0 || size == 0)
+		retirn (NULL);
+	if (mem == 0)
+		return (NULL);
+	for (i = 0; i < nmemb *size; i++)
+		*(mem + i) = 0;
+	return ((void *)mem);
+}
