c60f861399508bd2b09042b16bedc5fd43b01c8b
Marysyokau <mary.mbithe@students.jkuat.ac.ke>
2023-07-17 18:02:53 +0300

    0x0D-preprocessor

---
+++ my_practice/0-main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#inclule "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	Return (0);
+}
---
+++ my_practice/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - prints the standard error sandwitched around the output
+ *
+ * Return: returns 1
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\"
+}
