888bb799776b95641822a40eac7c2804df404113
Marysyokau <msyokau73@gmail.com>
2023-08-03 14:37:09 -0700

    bit manipulation clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,10 @@
  * Return: 1 if success, else -1
  */
 
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-
 	*n = (~(1UL << index) & *n);
 	return (1);
 }
